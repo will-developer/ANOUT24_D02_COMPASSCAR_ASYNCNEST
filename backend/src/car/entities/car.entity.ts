57f1a2c2ff7d214bf3e922f8316657e68008adf1
will-developer <william_limaa@hotmail.com>
2024-12-03 14:16:39 -0300

    feat: create Car entity structure

--- backend/src/car/entities/car.entity.ts
+++ backend/src/car/entities/car.entity.ts
@@ -1,1 +1,17 @@
-export class Car {}
+//import { Car } from '@prisma/client'
+
+export class Car /*implements Car*/ {
+  id: number;
+  brand: string;
+  model: string;
+  plate: string;
+  year: number;
+  kilometers: number;
+  dailyPrice: number;
+  status: boolean;
+  created_at: Date;
+  updated_at: Date;
+  inativatedAt?: Date | null;
+  // items: CarItem[]
+  // orders: Order[]
+}

059c54fd73169e2611e607556de6bd28b5278d83
will-developer <william_limaa@hotmail.com>
2024-12-03 13:24:12 -0300

    feat: add cars CRUD and setup DTO validation with class-validator

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CarModule } from './car/car.module';
 
 @Module({
-  imports: [],
+  imports: [CarModule],
   controllers: [AppController],
   providers: [AppService],
 })

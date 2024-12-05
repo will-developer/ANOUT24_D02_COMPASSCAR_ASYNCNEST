7fcd6c6913a064ef90e7aed5861a2d9ef8bb6fd3
Luan-1990 <luanldamasceno@hotmail.com>
2024-12-05 14:18:10 -0300

    feat prismaservice/appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/repository/order.module';
 
 @Module({
-  imports: [],
+  imports: [OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })

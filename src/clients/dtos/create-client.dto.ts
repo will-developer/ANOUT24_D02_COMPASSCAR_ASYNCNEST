9909a491c6c826e9e580e2492050612c9db7aa1f
regina guermandi <reginaguermandi@gmail.com>
2024-12-04 22:09:45 -0300

    feat: add descriptions and examples to swagger

--- src/clients/dtos/create-client.dto.ts
+++ src/clients/dtos/create-client.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import {
 	IsDate,
@@ -8,10 +9,18 @@ import {
 } from "class-validator";
 
 export class CreateClientDto {
+	@ApiProperty({
+		description: "Customer's full name",
+		example: "Jhon Doe",
+	})
 	@IsNotEmpty()
 	@IsString()
 	name: string;
 
+	@ApiProperty({
+		description: "Customer's CPF",
+		example: "123.456.789-10",
+	})
 	@IsNotEmpty()
 	@IsString()
 	@Matches(/^\d{3}(\.\d{3}){2}-\d{2}$|^\d{11}$/, {
@@ -19,15 +28,27 @@ export class CreateClientDto {
 	})
 	cpf: string;
 
+	@ApiProperty({
+		description: "Customer's date of birth",
+		example: "2024-01-01",
+	})
 	@IsNotEmpty()
 	@IsDate()
 	@Type(() => Date)
 	birthDate: Date;
 
+	@ApiProperty({
+		description: "Customer's e-mail",
+		example: "jhon@mail.com",
+	})
 	@IsNotEmpty()
 	@IsEmail()
 	email: string;
 
+	@ApiProperty({
+		description: "Customer's phone number",
+		example: "(11) 12345-6789",
+	})
 	@IsNotEmpty()
 	@Matches(/^\(\d{2}\)\s?\d{5}-\d{4}$/, {
 		message: "Phone must be in format: (11) 12345-6789",

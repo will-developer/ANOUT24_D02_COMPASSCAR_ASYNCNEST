05825fecc8dcd011df2214394d23232318c973c7
will-developer <william_limaa@hotmail.com>
2024-12-06 13:29:55 -0300

    chore: clean up code after merge

--- backend/src/car/dto/update-carItem.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCarItemDto } from './create-carItem.dto';
-
-export class UpdateCarItemDto extends PartialType(CreateCarItemDto) {}
--- prisma/migrations/20241206162657_update_at_in_car_table/migration.sql
+++ prisma/migrations/20241206162657_update_at_in_car_table/migration.sql
@@ -1,2 +1,5 @@
 -- AlterTable
 ALTER TABLE `car` MODIFY `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE `client` ALTER COLUMN `updatedAt` DROP DEFAULT;

7b017c538c27832c6cf5687e0a808b373f21a81c
will-developer <william_limaa@hotmail.com>
2024-12-07 15:10:24 -0300

    feat: add PartialType Swagger to update car and car item DTOs

--- src/car/dto/update-carItem.dto.ts
+++ src/car/dto/update-carItem.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateCarItemDto } from './create-carItem.dto';
 
 export class UpdateCarItemDto extends PartialType(CreateCarItemDto) {}

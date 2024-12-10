c33061d86d68881eef0fbd140a90c7de8b57ef9f
Mauro Ue <mauroue@gmail.com>
2024-12-10 13:45:44 -0300

    fix: correct some import errors

--- src/car/car.module.ts
+++ src/car/car.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { CarService } from './car.service';
 import { CarController } from './car.controller';
 import { CarRepository } from './repository/car.repository';
-import { PrismaService } from '../prisma/prisma.service';
 import { OrderModule } from '../order/repository/order.module';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
   imports: [OrderModule],

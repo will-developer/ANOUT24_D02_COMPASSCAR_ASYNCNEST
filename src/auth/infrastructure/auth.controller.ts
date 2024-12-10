1f3d7be1a0e4b1839072695f63db307ae115e9b4
Mauro Ue <mauroue@gmail.com>
2024-12-09 21:47:24 -0300

    refactor: remove unused method

--- src/auth/infrastructure/auth.controller.ts
+++ src/auth/infrastructure/auth.controller.ts
@@ -1,10 +1,7 @@
-import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from '../application/services/auth.services';
 import { SignInDto } from '../application/dtos/sign-in.dto';
-import { CreateUserDTO } from 'src/users/dto/create-user.dto';
-import { UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -20,18 +17,5 @@ export class AuthController {
   @Post('signin')
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
-  }
-
-  @ApiBearerAuth()
-  @ApiOperation({ summary: 'Register new user' })
-  @ApiResponse({ 
-    status: 201, 
-    description: 'User successfully created'
-  })
-  @ApiResponse({ status: 409, description: 'Email already registered' })
-  @UseGuards(JwtAuthGuard)
-  @Post('signup')
-  signUp(@Body() createUserDto: CreateUserDTO) {
-    return this.authService.signUp(createUserDto);
   }
 }

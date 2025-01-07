6cd33ab2cadde771bc451df414d1e8a11febb723
William Augusto de Lima <160856926+will-developer@users.noreply.github.com>
2025-01-07 10:42:00 -0300

    update: src/app.service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Git Hub Actions ON With Auto-Deploy AWS!';
   }
 }
